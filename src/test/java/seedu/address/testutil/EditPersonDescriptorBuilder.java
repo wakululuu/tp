c32d6d09f6c3f7497c059c4962e0a988b148660a
Sigmund <sigmundchianasta@gmail.com>
2020-10-04 18:41:56 +0800

    change email to pay

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -6,8 +6,9 @@ import java.util.stream.Stream;
 
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
+//import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Pay;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
@@ -34,7 +35,8 @@ public class EditPersonDescriptorBuilder {
         descriptor = new EditPersonDescriptor();
         descriptor.setName(person.getName());
         descriptor.setPhone(person.getPhone());
-        descriptor.setEmail(person.getEmail());
+        descriptor.setPay(person.getPay());
+//        descriptor.setEmail(person.getEmail());
         descriptor.setAddress(person.getAddress());
         descriptor.setTags(person.getTags());
     }
@@ -56,12 +58,20 @@ public class EditPersonDescriptorBuilder {
     }
 
     /**
-     * Sets the {@code Email} of the {@code EditPersonDescriptor} that we are building.
+     * Sets the {@code Pay} of the {@code EditPersonDescriptor} that we are building.
      */
-    public EditPersonDescriptorBuilder withEmail(String email) {
-        descriptor.setEmail(new Email(email));
+    public EditPersonDescriptorBuilder withPay(String pay) {
+        descriptor.setPay(new Pay(pay));
         return this;
     }
+
+//    /**
+//     * Sets the {@code Email} of the {@code EditPersonDescriptor} that we are building.
+//     */
+//    public EditPersonDescriptorBuilder withEmail(String email) {
+//        descriptor.setEmail(new Email(email));
+//        return this;
+//    }
 
     /**
      * Sets the {@code Address} of the {@code EditPersonDescriptor} that we are building.
@@ -84,4 +94,5 @@ public class EditPersonDescriptorBuilder {
     public EditPersonDescriptor build() {
         return descriptor;
     }
+
 }
