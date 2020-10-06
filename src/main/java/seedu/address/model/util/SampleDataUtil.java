c46908c001a7a744a545e1f0761737679a488fc4
Sigmund <sigmundchianasta@gmail.com>
2020-10-06 17:31:47 +0800

    fix inital sample data to have proper Pay amounts

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -24,22 +24,22 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Pay("alexyeoh@example.com"),
+            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Pay("10"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
                 getTagSet("friends")),
-            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Pay("berniceyu@example.com"),
+            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Pay("11.2"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                 getTagSet("colleagues", "friends")),
-            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Pay("charlotte@example.com"),
+            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Pay("9.87"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
                 getTagSet("neighbours")),
-            new Person(new Name("David Li"), new Phone("91031282"), new Pay("lidavid@example.com"),
+            new Person(new Name("David Li"), new Phone("91031282"), new Pay("0.99"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
                 getTagSet("family")),
-            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Pay("irfan@example.com"),
+            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Pay("23"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
                 getTagSet("classmates")),
-            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Pay("royb@example.com"),
+            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Pay("11.10"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
                 getTagSet("colleagues"))
         };
