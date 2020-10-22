121b82257e84bfb487b2f504e30dfef0af5505bc
Foo Kai En <kaien.foo@gmail.com>
2020-10-22 21:40:19 +0800

    Resolve merge conflict

--- src/main/java/seedu/address/logic/commands/AssignCommand.java
+++ src/main/java/seedu/address/logic/commands/AssignCommand.java
@@ -9,6 +9,7 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_SHIFTS;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_WORKERS;
 
 import java.util.List;
+import java.util.Set;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
