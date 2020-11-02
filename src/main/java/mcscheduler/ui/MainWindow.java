25bfe3fd2a0d9ba20e7f2fb43a9dc70392b96ec0
Foo Kai En <kaien.foo@gmail.com>
2020-11-02 20:55:19 +0800

    Add role panel

--- src/main/java/mcscheduler/ui/MainWindow.java
+++ src/main/java/mcscheduler/ui/MainWindow.java
@@ -33,6 +33,7 @@ public class MainWindow extends UiPart<Stage> {
     // Independent Ui parts residing in this Ui container
     private WorkerListPanel workerListPanel;
     private ShiftListPanel shiftListPanel;
+    private RoleListPanel roleListPanel;
     private ResultDisplay resultDisplay;
     private final HelpWindow helpWindow;
 
@@ -47,6 +48,9 @@ public class MainWindow extends UiPart<Stage> {
 
     @FXML
     private StackPane shiftListPanelPlaceholder;
+
+    @FXML
+    private StackPane roleListPanelPlaceholder;
 
     @FXML
     private StackPane resultDisplayPlaceholder;
@@ -119,6 +123,9 @@ public class MainWindow extends UiPart<Stage> {
 
         shiftListPanel = new ShiftListPanel(logic.getFilteredShiftList(), logic.getFullAssignmentList());
         shiftListPanelPlaceholder.getChildren().add(shiftListPanel.getRoot());
+
+        roleListPanel = new RoleListPanel(logic.getFilteredRoleList());
+        roleListPanelPlaceholder.getChildren().add(roleListPanel.getRoot());
 
         resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
