fcc1e021d0b47cbbaa429d0c1118b1e1239003c3
Tan Su Yin <e0425292@u.nus.edu>
2020-11-06 22:19:58 +0800

    Fix checkstyle errors

--- src/main/java/mcscheduler/storage/JsonAdaptedUnavailability.java
+++ src/main/java/mcscheduler/storage/JsonAdaptedUnavailability.java
@@ -10,15 +10,26 @@ public class JsonAdaptedUnavailability {
 
     private final String unavailability;
 
+    /**
+     * Constructs a {@code JsonAdaptedUnavailability} with the given {@code unavailability}.
+     */
     @JsonCreator
     public JsonAdaptedUnavailability(@JsonProperty("unavailability") String unavailability) {
         this.unavailability = unavailability;
     }
 
+    /**
+     * Converts a given {@code Unavailability} into this class for Jackson use.
+     */
     public JsonAdaptedUnavailability(Unavailability source) {
         this.unavailability = source.getString();
     }
 
+    /**
+     * Converts this Jackson-friendly adapted unavailability object into the model's {@code Unavailability} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted unavailability.
+     */
     public Unavailability toModelType() throws IllegalValueException {
         if (!Unavailability.isValidUnavailability(unavailability)) {
             throw new IllegalValueException((Unavailability.MESSAGE_CONSTRAINTS));
